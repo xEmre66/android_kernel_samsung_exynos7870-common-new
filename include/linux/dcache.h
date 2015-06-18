0e237aa66157799fa28363b953d09eb23ac7e74d
David Howells <dhowells@redhat.com>
2015-06-18 14:32:31 +0100

    overlayfs: Make f_path always point to the overlay and f_inode to the underlay
    [ Upstream commit 4bacc9c9234c7c8eec44f5ed4e960d9f96fa0f01 ]
    
    Make file->f_path always point to the overlay dentry so that the path in
    /proc/pid/fd is correct and to ensure that label-based LSMs have access to the
    overlay as well as the underlay (path-based LSMs probably don't need it).
    
    Using my union testsuite to set things up, before the patch I see:
    
    	[root@andromeda union-testsuite]# bash 5</mnt/a/foo107
    	[root@andromeda union-testsuite]# ls -l /proc/$$/fd/
    	...
    	lr-x------. 1 root root 64 Jun  5 14:38 5 -> /a/foo107
    	[root@andromeda union-testsuite]# stat /mnt/a/foo107
    	...
    	Device: 23h/35d Inode: 13381       Links: 1
    	...
    	[root@andromeda union-testsuite]# stat -L /proc/$$/fd/5
    	...
    	Device: 23h/35d Inode: 13381       Links: 1
    	...
    
    After the patch:
    
    	[root@andromeda union-testsuite]# bash 5</mnt/a/foo107
    	[root@andromeda union-testsuite]# ls -l /proc/$$/fd/
    	...
    	lr-x------. 1 root root 64 Jun  5 14:22 5 -> /mnt/a/foo107
    	[root@andromeda union-testsuite]# stat /mnt/a/foo107
    	...
    	Device: 23h/35d Inode: 40346       Links: 1
    	...
    	[root@andromeda union-testsuite]# stat -L /proc/$$/fd/5
    	...
    	Device: 23h/35d Inode: 40346       Links: 1
    	...
    
    Note the change in where /proc/$$/fd/5 points to in the ls command.  It was
    pointing to /a/foo107 (which doesn't exist) and now points to /mnt/a/foo107
    (which is correct).
    
    The inode accessed, however, is the lower layer.  The union layer is on device
    25h/37d and the upper layer on 24h/36d.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>

--- include/linux/dcache.h
+++ include/linux/dcache.h
@@ -160,6 +160,7 @@ struct dentry_operations {
 	char *(*d_dname)(struct dentry *, char *, int);
 	struct vfsmount *(*d_automount)(struct path *);
 	int (*d_manage)(struct dentry *, bool);
+	struct inode *(*d_select_inode)(struct dentry *, unsigned);
 	void (*d_canonical_path)(const struct path *, struct path *);
 } ____cacheline_aligned;
 
@@ -224,6 +225,7 @@ struct dentry_operations {
 
 #define DCACHE_MAY_FREE			0x00800000
 #define DCACHE_WILL_INVALIDATE		0x80000000 /* will be invalidated */
+#define DCACHE_OP_SELECT_INODE		0x02000000 /* Unioned entry: dcache op selects inode */
 
 extern seqlock_t rename_lock;
 
