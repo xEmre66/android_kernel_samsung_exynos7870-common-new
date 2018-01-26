5f10dfc78cf07bedcb6f06e154cf29858ffc8828
DarkLord1731 <darklord1731@gmail.com>
2018-01-26 19:47:48 +0530

    Revert "ZEN: Implement Zen-tune v4.5"
    This reverts commit a50bdfe7eb4d7c8c8329e979de794b195fa88585.

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -39,13 +39,7 @@ struct blkcg_gq;
 struct blk_flush_queue;
 
 #define BLKDEV_MIN_RQ	4
-#if defined (CONFIG_ZEN_INTERACTIVE)
-#define BLKDEV_MAX_RQ	512
-#elif definded (CONFIG_LARGE_DIRTY_BUFFER)
-#define BLKDEV_MAX_RQ	256
-#else
-#define BLKDEV_MAX_RQ  128     /* Default maximum */
-#endif
+#define BLKDEV_MAX_RQ	128	/* Default maximum */
 
 /*
  * Maximum number of blkcg policies allowed to be registered concurrently.
